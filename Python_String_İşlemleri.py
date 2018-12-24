a3555517affcb2735babdd94b78ce78439e34124
gorki2 <30976571+gorki2@users.noreply.github.com>
2018-12-24 20:05:43 +0300

    Create Python_String_İşlemleri.py

---
+++ Python_String_İşlemleri.py
@@ -0,0 +1,13 @@
+#Python Stringler İle İşlemler#
+
+print("String İşlemleri Örneği") # Yazı 
+ad = input("Adınız: ") # Program Çalıştığında ad Değişkenine Herhangi Bir Değer Atarız.
+soyad = input("Soyadınız: ") # Program Çalıştığında soyad Değişkenine Herhangi Bir Değer Atarız.
+dogumt = input("Doğum Tarihi: ") # Program Çalıştığında dogumt Değişkenine Herhangi Bir Değer Atarız.
+dogumy = input("Doğum Yeri: ") # Program Çalıştığında dogumy Değişkenine Herhangi Bir Değer Atarız.
+print("=====Bilgiler=====") # Yazı
+print("Adınız: " + ad) # Adınız: yazısını Ve ad değişkenindeki değeri ekrana yazdırır.
+print("Soyadınız: " + soyad) # Soyadınız: yazısını Ve soyad değişkenindeki değeri ekrana yazdırır.
+print("Doğum Tarihi: " + dogumt) # Doğum Tarihi yazısını Ve dogumt değişkenindeki değeri ekrana yazdırır.
+print("Doğum Yeri: " + dogumy) # Doğum Yeri yazısını Ve dogumy değişkenindeki değeri ekrana yazdırır.
+print("Bilgileriniz Bunlar Kayıt Tamam!") # Yazı
