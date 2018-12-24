5f5c66b5adca00183b41a464ea3b14ed41b3050e
gorki2 <30976571+gorki2@users.noreply.github.com>
2018-12-24 20:04:55 +0300

    Create Python_İf_Else_Ornek.py

---
+++ Python_İf_Else_Ornek.py
@@ -0,0 +1,8 @@
+# Python İf Else Örnek Program
+
+a = int(input("Bir Değer Girin Sayı")) # a Değişkenine Bir Değer Verilir.
+if (a < 10): #Eğer a  Değeri 10 dan Küçükse Alttaki Metin Çıkacak.
+ print("Sayı 10 dan Küçük!") #Yazı
+
+else: # Ama a değeri 10 dan büyükse aşağıdaki metin çıkacaktır.  
+	print("Sayı 10 dan Büyük!") #Yazı
