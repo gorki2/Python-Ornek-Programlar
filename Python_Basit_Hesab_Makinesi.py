e2895c6135c87cc69178a7f18ba1851f082ca415
gorki2 <30976571+gorki2@users.noreply.github.com>
2019-01-21 18:59:42 +0300

    Delete Python_Basit_Hesab_Makinesi.py

--- Python_Basit_Hesab_Makinesi.py
+++
@@ -1,7 +0,0 @@
-#Python Basit Hesap Makinesi#
-
-sayi1 = int(input("Sayı 1: ")) # Sayı 1 i İnt Değeri Şelinde Kaydeder.#
-sayi2 = int(input("Sayı 2: ")) # Sayı 2 yi İnt Değeri Şelinde Kaydeder.#
-sonuc = (sayi1 + sayi2) # Sayı 1 Ve Sayı 2 Değerini sonuc Değişkeni Altında Toplar.#
-print("Cevab;") #Cevab Yazısı#
-print(sonuc) #İki Sayının Toplamını Sonuc değişkeni üzerinde ekrana yansıtır.#
